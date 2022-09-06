317d730bd534931bf6cc6d713e23a55c2075449d
TARAKKONDURI <95854347+TARAKKONDURI@users.noreply.github.com>
2022-09-06 22:08:54 +0530

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,22 +11,22 @@ export class ProductService {
   constructor(private http:HttpClient) { }
 
   saveProduct(product: Product): Observable<any> {
-    return this.http.post("http://3.109.48.104:8080/product", product);
+    return this.http.post("https://pms-sb-code.herokuapp.com/product", product);
   }
 
   saveChanges(product: Product):Observable<any>{
-    return this.http.put("http://3.109.48.104:8080/product", product);
+    return this.http.put("https://pms-sb-code.herokuapp.com/product", product);
   }
 
   getAllProducts(): Observable<any> {
-    return this.http.get("http://3.109.48.104:8080/products");
+    return this.http.get("https://pms-sb-code.herokuapp.com/products");
   }
 
   deleteProduct(id:number):Observable<Object>{
-    return this.http.delete("http://3.109.48.104:8080/product/"+id);
+    return this.http.delete("https://pms-sb-code.herokuapp.com/product/"+id);
   }
 
   getProductById(id:number):Observable<any>{
-    return this.http.get("http://3.109.48.104:8080/product/"+id);
+    return this.http.get("https://pms-sb-code.herokuapp.com/product/"+id);
   }
 }
